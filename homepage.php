38aaecb190ceed66b0f7df4866ac1692e755d4d1
zmarsh2000 <72839361+zmarsh2000@users.noreply.github.com>
2020-10-13 19:50:41 -0400

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Marshall Management Company, LLC</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <header class="container">
+    <div class="row">
+      <h1 class="col-sm-8">Marshall Management Company</h1>
+      <nav class="col-sm-4">
+        <p>Residential Properties</p>
+        <p>Commercial Properties</p>
+        <p><a href="pages/aboutus.php">About Us</p>
+      </nav>
+    </div>
+  </header>
+</body>
+
+<html>
+
---
+++ pages/aboutus.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<title>MMCO | About Us</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <header class="container">
+    <div class="row">
+      <h1 class="col-sm-8">Marshall Management Company</h1>
+      <nav class="col-sm-4">
+        <p>Residential Properties</p>
+        <p>Commercial Properties</p>
+        <p><a href="pages/aboutus.php">About Us</p>
+      </nav>
+    </div>
+  </header>
+  <section class="container">
+  <div class="row">
+  <h3 class="col-sm-4">About Us</h3>
+  <body>
+  Marshall Management Company is an innovator and corporate leader in a myriad of industries including real estate, steam generators, and publishing.
+  </body>
+  </div>
+  </section>
+</body>
+
+<html>
